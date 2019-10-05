36d0f80ed883435d82bda7275f558a1ca88b462f
Jeritens <20913496+Jeritens@users.noreply.github.com>
2019-10-05 20:51:18 +0200

    changed weird conflict stuff

--- LudumDare_45/Assets/Scipts/CameraBehaviour.cs
+++ LudumDare_45/Assets/Scipts/CameraBehaviour.cs
@@ -6,11 +6,11 @@ public class CameraBehaviour : MonoBehaviour
 {
     public GameObject ball;
     [SerializeField]
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(3,0,-10);
     [SerializeField]
-    private float maxSpeed = 5f;
+    private float maxSpeed = 5;
     [SerializeField]
-    private float maxDistance = 7f;
+    private float maxDistance = 7;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +19,7 @@ public class CameraBehaviour : MonoBehaviour
 
     void LateUpdate()
     {   
-        Vector3  destination = new Vector3(ball.transform.position.x,Mathf.Max(0,ball.transform.position.y),0)+offset;
+        Vector3  destination = new Vector3(ball.transform.position.x,Mathf.Max(4,ball.transform.position.y),0)+offset;
         //camera damping
         float distance = Mathf.Min((destination-transform.position).magnitude,maxDistance);
         float speed = distance/maxDistance*maxSpeed;
