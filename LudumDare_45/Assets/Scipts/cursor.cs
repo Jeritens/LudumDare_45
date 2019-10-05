b078b3c2074f82da7637fed1aa70739929d1c7d4
Beritens <34006542+Beritens@users.noreply.github.com>
2019-10-06 01:08:22 +0200

    graphics

---
+++ LudumDare_45/Assets/Scipts/cursor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursor : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible= false;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector2 cursorPos= Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+
+    }
+}
---
+++ LudumDare_45/Assets/background.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background : MonoBehaviour
+{
+    SpriteRenderer sR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sR = GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        Vector2 parentPos = transform.position-transform.localPosition;
+        transform.localPosition= new Vector3(-parentPos.x%transform.localScale.x,-parentPos.y%transform.localScale.y,10);
+    }
+}
