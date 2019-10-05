600aee4e9e4f2d263d0fc471841560f66c4eddb2
Jeritens <20913496+Jeritens@users.noreply.github.com>
2019-10-05 20:26:27 +0200

    camera follow and damping

---
+++ LudumDare_45/Assets/Scipts/CameraBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public GameObject ball;
+    [SerializeField]
+    private Vector3 offset;
+    [SerializeField]
+    private float maxSpeed = 5f;
+    [SerializeField]
+    private float maxDistance = 7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void LateUpdate()
+    {   
+        Vector3  destination = new Vector3(ball.transform.position.x,Mathf.Max(0,ball.transform.position.y),0)+offset;
+        //camera damping
+        float distance = Mathf.Min((destination-transform.position).magnitude,maxDistance);
+        float speed = distance/maxDistance*maxSpeed;
+        transform.position = Vector3.Lerp(transform.position, destination, speed* Time.deltaTime);
+
+    }
+}
