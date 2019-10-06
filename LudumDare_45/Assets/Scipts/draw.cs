51e58964704d6fbc641aaf37b426b9fdf81ff2f3
Jeritens <20913496+Jeritens@users.noreply.github.com>
2019-10-06 17:36:59 +0200

    powe up speed line

--- LudumDare_45/Assets/Scipts/draw.cs
+++ LudumDare_45/Assets/Scipts/draw.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class draw : MonoBehaviour
 {
     public float ink;
+    public float redInk;
     public GameObject linePrefab;
+    public GameObject speedLinePrefab;
     LineRenderer currentLineR;
     EdgeCollider2D currentLineCol;
     void Start(){
@@ -21,16 +23,44 @@ public class draw : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {        
         if(Input.GetButtonDown("Fire1")&&ink>0){
-                GameObject line = GameObject.Instantiate(linePrefab,Vector3.zero,Quaternion.identity);
-                currentLineR = line.GetComponent<LineRenderer>();
-                currentLineCol = line.GetComponent<EdgeCollider2D>();
+            StartLine();
         }
         if(Input.GetButton("Fire1")&&ink>0){
-            
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            DrawLine();
+        }
+        else if(currentLineR != null){
+            StopLine();
+        }
+        
+    }
+
+    public void AddRedInk(float amountInk){
+        redInk += amountInk;
+        if(currentLineR != null){
+            StopLine();
+            StartLine();
+        }
+    }
+
+
+    private void StartLine(){
+        if(redInk>0){
+            GameObject line = GameObject.Instantiate(speedLinePrefab,Vector3.zero,Quaternion.identity);
+            currentLineR = line.GetComponent<LineRenderer>();
+            currentLineCol = line.GetComponent<EdgeCollider2D>();
+        }
+        else{
+            GameObject line = GameObject.Instantiate(linePrefab,Vector3.zero,Quaternion.identity);
+            currentLineR = line.GetComponent<LineRenderer>();
+            currentLineCol = line.GetComponent<EdgeCollider2D>();
+        }
+        DrawLine();
+    }
+
+    private void DrawLine(){
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             currentLineR.positionCount++;
             currentLineR.SetPosition(currentLineR.positionCount-1,mousePos);
             Vector2[] points= new Vector2[currentLineR.positionCount];
@@ -38,14 +68,23 @@ public class draw : MonoBehaviour
                 points[i]=(Vector2)currentLineR.GetPosition(i);
             }
             currentLineCol.points = points;
-            if(currentLineR.positionCount>1)
-                ink-=Vector2.Distance(currentLineR.GetPosition(currentLineR.positionCount-1),currentLineR.GetPosition(currentLineR.positionCount-2));
-        }
-        else if(currentLineR != null){
-            currentLineR.GetComponent<desolve>().startTimer();
-            currentLineR = null;
-            currentLineCol = null;
-        }
-        
+            if(currentLineR.positionCount>1){
+                float distance = Vector2.Distance(currentLineR.GetPosition(currentLineR.positionCount-1),currentLineR.GetPosition(currentLineR.positionCount-2));
+                ink-= distance;
+                if(redInk > 0){
+                    redInk -= distance;
+                    if(redInk<=0){
+                        StopLine();
+                        StartLine();
+                        redInk = 0;
+                    }
+                }
+            }
+    }
+
+    private void StopLine(){
+        currentLineR.GetComponent<desolve>().startTimer();
+        currentLineR = null;
+        currentLineCol = null;
     }
 }
