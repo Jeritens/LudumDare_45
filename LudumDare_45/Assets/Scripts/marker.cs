31df3107ba3c2c9eda8782d2a48e067f0c8255bf
Beritens <34006542+Beritens@users.noreply.github.com>
2019-10-07 18:43:40 +0200

    HSLine

--- LudumDare_45/Assets/Scripts/marker.cs
+++ LudumDare_45/Assets/Scripts/marker.cs
@@ -6,10 +6,17 @@ using TMPro;
 public class marker : MonoBehaviour
 {
     public TextMeshPro text;
+    public LineRenderer lr;
+    public Color pencil;
 
-    public void SetPos(int pos){
+    public void SetPos(int pos, bool ink){
         //transform.position = Vector2.right*pos;
         text.text = pos.ToString();
+        if(!ink){
+            text.color= pencil;
+            lr.startColor= pencil;
+            lr.endColor= pencil;
+        }
     }
     public int GetPos(){
         return (int)transform.position.x;
--- LudumDare_45/Assets/Scripts/scoreLine.cs
+++ LudumDare_45/Assets/Scripts/scoreLine.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class scoreLine : MonoBehaviour
 {
@@ -9,19 +10,28 @@ public class scoreLine : MonoBehaviour
     public Transform line;
     public Transform markerParent;
     public List<marker> markers = new List<marker>();
+    public LineRenderer pencil;
+    public Transform HSMarker;
+    public TextMeshPro HSText;
+    float hs;
     // Start is called before the first frame update
     void Start()
     {
+        hs = PlayerStats.stats.GetMaxInk();
+        HSMarker.position= Vector2.right*hs;
+        HSText.text= hs.ToString("0.0");
+        pencil.SetPosition(1,Vector2.right*hs);
         for(int i = 0; i<8; i++){
             GameObject g = GameObject.Instantiate(markerPrefab,Vector3.right*i*5,Quaternion.identity,markerParent);
             marker m = g.GetComponent<marker>();
-            m.SetPos(i*5);
+            m.SetPos(i*5,i*5>hs);
             markers.Add(m);
         }
+        
     }
     void rewritemarkers(int pos){
         for(int i = 0; i<markers.Count; i++){
-            markers[i].SetPos(pos+i*5);
+            markers[i].SetPos(pos+i*5,pos+i*5>hs);
         }
     }
     
@@ -43,7 +53,7 @@ public class scoreLine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        line.position = Vector2.right* Mathf.Max((cam.position.x-20),0);
+        line.position = Vector2.right* Mathf.Max((cam.position.x-20),hs);
         if(Mathf.Abs(cam.position.x-(markerParent.position.x+10))>5){
             int pos = Mathf.Max(Mathf.FloorToInt((cam.position.x-10f)/5f)*5,0);
             markerParent.position = Vector2.right*pos;
