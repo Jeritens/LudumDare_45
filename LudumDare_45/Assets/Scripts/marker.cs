7801e4f2c2262dad9c75e8ff8f3b14f803cd3526
Beritens <34006542+Beritens@users.noreply.github.com>
2019-10-09 17:42:12 +0200

    small bugfix
    marker doesn't change color back.

--- LudumDare_45/Assets/Scripts/marker.cs
+++ LudumDare_45/Assets/Scripts/marker.cs
@@ -8,6 +8,15 @@ public class marker : MonoBehaviour
     public TextMeshPro text;
     public LineRenderer lr;
     public Color pencil;
+    Color normal;
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        normal = lr.startColor;
+    }
 
     public void SetPos(int pos, bool ink){
         //transform.position = Vector2.right*pos;
@@ -16,6 +25,11 @@ public class marker : MonoBehaviour
             text.color= pencil;
             lr.startColor= pencil;
             lr.endColor= pencil;
+        }
+        else{
+            text.color= normal;
+            lr.startColor= normal;
+            lr.endColor= normal;
         }
     }
     public int GetPos(){
